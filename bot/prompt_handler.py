6d3403aa4558d51aa7cb4a841bbd9451d1526542
Mouad Fiali <fialimouad@gmail.com>
2023-10-27 01:25:30 +0200

    fix: fixed the environment variables access and added a usage example

--- bot/prompt_handler.py
+++ bot/prompt_handler.py
@@ -1,6 +1,24 @@
 import os
+from dotenv import load_dotenv
 import openai
-openai.organization = "org-u6md6sQAo6TxNMmQoGeq4WpY"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+load_dotenv()
+
+openai.organization = os.environ["ORGANIZATION_ID"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+print(openai.organization)
 print(openai.api_key)
-print(openai.Model.list())
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "user", "content": "Hello, I'm a human"
+        }
+    ]
+)
+
+
+print(response["choices"][0]["message"]["content"])
+
